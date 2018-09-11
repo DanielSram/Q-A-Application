9bb76235ca22ebc674cdffc74c01a03c74cda829
Daniel Sram <sramdaniel@gmail.com>
2018-09-11 14:32:40 +0200

    remove unused dependency

--- src/main/java/cz/cd/bootcamp/qa/QaApplication.java
+++ src/main/java/cz/cd/bootcamp/qa/QaApplication.java
@@ -3,10 +3,15 @@ package cz.cd.bootcamp.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * App starting point.
+ *
+ * @author Daniel Sram
+ */
 @SpringBootApplication
 public class QaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QaApplication.class, args);
+    }
 }
